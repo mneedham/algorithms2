61a48c4058cb993277ac19a8b70c32ce1c1725fb
Mark Needham <m.h.needham@gmail.com>
2012-12-08 16:08:27 +0000

    mark: more attempts at readability

--- jobs.rb
+++ jobs.rb
@@ -1,26 +1,30 @@
 SORTING_METHODS = {
   :by_decreasing_order_of_distance => ->(one,two) {
-    one_difference = (one[0] - one[1])
-    two_difference = (two[0] - two[1])
+    one_difference = (one[:weight] - one[:length])
+    two_difference = (two[:weight] - two[:length])
 
     if one_difference == two_difference
-      two[0] <=> one[0]
+      two[:weight] <=> one[:weight]
     else
       two_difference <=> one_difference
     end  
   },
   
   :by_decreasing_order_of_ratio => ->(one,two) {
-    one_ratio = (one[0] / one[1].to_f)
-    two_ratio = (two[0] / two[1].to_f)  
+    one_ratio = (one[:weight] / one[:length].to_f)
+    two_ratio = (two[:weight] / two[:length].to_f)  
     two_ratio <=> one_ratio
   }
 }
 
 [:by_decreasing_order_of_distance, :by_decreasing_order_of_ratio].each do |sort_method|
-  pairs = File.readlines("small_jobs.txt").drop(1).map { |x| x.gsub(/\n/, "").split(" ").map(&:to_i) }.sort(& SORTING_METHODS[sort_method])
+  pairs = File.readlines("small_jobs.txt").drop(1).map do  |x| 
+    (weight, length) = x.gsub(/\n/, "").split(" ").map(&:to_i)
+    { :weight => weight, :length => length }
+  end.sort(& SORTING_METHODS[sort_method])
 
-  total = pairs.inject({:total => 0, :time_so_far => 0}) do |acc, (weight, length)| 
+  total = pairs.inject({:total => 0, :time_so_far => 0}) do |acc, row| 
+    weight, length = row[:weight], row[:length]
     new_total = acc[:total] + ((acc[:time_so_far] + length) * weight)
     new_time_so_far = acc[:time_so_far] + length
     {:total => new_total, :time_so_far => new_time_so_far }
