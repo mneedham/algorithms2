badc3948464fc57cafa7077fcacdb360def3a7a2
Mark Needham <m.h.needham@gmail.com>
2012-12-20 21:34:07 +0000

    mark: renaming UnionSet to proper name and adding clustering solution

--- clustering.rb
+++ clustering.rb
@@ -1,20 +1,9 @@
-#Kruskal's algorithm
-def header 
-  @header ||= file.take(1)[0]
-end
-
-def number_of_nodes
-  @number_of_nodes ||= header.split(" ")[0].to_i
-end
-
-def file 
-  @file ||= File.readlines("edges.txt")
-end
+require 'set'
 
 # Some renaming of variables of Michael Luckender's class -> https://github.com/mluckeneder/Union-Find-Ruby/blob/master/quick-union.rb
 class UnionFind
   def initialize(n)
-    @leaders = 1.upto(n).inject([]) { |leaders, i| leaders[i] = i; leaders }
+    @leaders = 0.upto(n-1).inject([]) { |leaders, i| leaders[i] = i; leaders }
   end
   
   def connected?(id1,id2)
@@ -25,22 +14,42 @@ class UnionFind
     leader_1, leader_2 = @leaders[id1], @leaders[id2]
     @leaders.map! {|i| (i == leader_1) ? leader_2 : i }
   end
+  
+  def number_of_clusters
+    Set.new(@leaders).size
+  end
+  
+  def cluster_leaders
+    Set.new(@leaders)
+  end
+end
+
+def file 
+  @file ||= File.readlines("clustering1.txt")
+end
+
+def number_of_nodes
+  @number_of_nodes ||= header.split(" ")[0].to_i
+end
+
+def header 
+  @header ||= file.take(1)[0]
 end
 
 set = UnionFind.new number_of_nodes
 
-@minimum_spanning_tree = []
-
 edges = file.drop(1).map { |x| x.gsub(/\n/, "").split(" ").map(&:to_i) }.
                      map { |one, two, weight| { :from => one, :to => two, :weight => weight}}.
                      sort_by { |x| x[:weight]}
-                     
+           
+spacing = nil                     
 edges.each do |edge|
-  if !set.connected?(edge[:from], edge[:to])
-    @minimum_spanning_tree << edge 
-    set.union(edge[:from], edge[:to])
-  end  
+  if set.number_of_clusters > 4
+    set.union(edge[:from]-1, edge[:to]-1)
+  else
+    spacing = edge[:weight]
+    break
+  end
 end
 
-puts "MST: #{@minimum_spanning_tree}"
-puts "Cost: #{@minimum_spanning_tree.inject(0) { |acc, x| acc + x[:weight]}}"
+puts "spacing: #{spacing}"
