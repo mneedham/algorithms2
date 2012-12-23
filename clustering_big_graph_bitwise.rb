b6b296cdf86261c74cfaa64db8557c5f6a53d10f
Mark Needham <m.h.needham@gmail.com>
2012-12-23 02:53:39 +0000

    mark: now just use the decimal version instead of arrays

--- clustering_big_graph_bitwise.rb
+++ clustering_big_graph_bitwise.rb
@@ -37,25 +37,27 @@ def header
   @header ||= file.take(1)[0]
 end
 
-def close_friends(me)
-  as_base_10 = me.join("").to_i(2)
-  offsets = (0..(me.size - 1)).map { |x| 2 ** x }
+def bits 
+  24
+end
+
+def close_friends(as_base_10, bits)
+  offsets = (0..(bits - 1)).map { |x| 2 ** x }
   (offsets.map { |off| as_base_10 ^ off } + offsets.combination(2).to_a.map { |a,b| as_base_10 ^ (a|b) }) + [as_base_10]
 end
 
 set = UnionFind.new number_of_nodes
 
-nodes = file.drop(1).map { |x| x.gsub(/\n/, "").split(" ").map(&:to_i) }
+nodes = file.drop(1).map { |x| x.gsub(/\n/, "").gsub(/ /, "").to_i(2)}
 
 @magical_hash = {}
 nodes.each_with_index do |node, index|
-  as_base_10 = node.join("").to_i(2)
-  @magical_hash[as_base_10] ||= []
-  @magical_hash[as_base_10] << index
+  @magical_hash[node] ||= []
+  @magical_hash[node] << index
 end
 
 nodes.each_with_index do |node, index|  
-  close_friends(node).each do |friend|
+  close_friends(node, bits).each do |friend|
     (@magical_hash[friend] || []).each { |friend_index| set.union(index, friend_index) }
   end
 end
