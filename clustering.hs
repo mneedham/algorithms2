a0c12a19794f595490ca198f2600b368d49d7273
Mark Needham <m.h.needham@gmail.com>
2012-12-30 22:32:45 +0000

    mark: pattern matching it up

--- clustering.hs
+++ clustering.hs
@@ -10,6 +10,8 @@ import qualified Data.Map as Map
 import qualified Data.Set as Set
 import qualified Data.List as List
 import Data.Maybe as Maybe
+
+import Debug.Trace
     
 -- subsets of size k
 combinationsOf 0 _ = [[]]
@@ -17,19 +19,20 @@ combinationsOf _ [] = []
 combinationsOf k (x:xs) = map (x:) (combinationsOf (k-1) xs) ++ combinationsOf k xs
 
 maxCluster :: Int -> [Int] -> UnionSet Int -> Map.Map Int Int -> Int
+-- maxCluster :: Int -> [Int] -> Equivalence Int -> Map.Map Int Int -> Int
 maxCluster bits nodes unionFind nodesMap = 
-    numberOfComponents $ foldl (\uf (x,y) -> Leaders.union uf x y ) unionFind (nodesToMerge nodes nodesMap offsets)
+    -- numberOfComponents $ foldl (\uf (x,y) -> equate x y uf) unionFind (nodesToMerge nodes nodesMap offsets)
+    numberOfComponents $ foldl (\uf (x,y) -> Leaders.union uf x y) unionFind (nodesToMerge nodes nodesMap offsets)    
     where offsets = map (shiftL 1) [0..(bits - 1)]
 
 nodesToMerge :: [Int] -> Map.Map Int Int -> [Int] -> [(Int, Int)]
 nodesToMerge nodes nodesMap offsets = 
-    List.sort . Set.toList . Set.fromList . map smallestIdFirst . concatMap nodeCombinations $ (zip [0..] nodes)    
-    -- List.sort . map smallestIdFirst . concatMap nodeCombinations $ (zip [0..] nodes)    
+    List.sort . Set.toList . Set.fromList . map smallestIdFirst . concatMap nodeCombinations $ (zip [0..] nodes)     
     where nodeCombinations (nodeIndex, node) = zip (repeat nodeIndex) (getNeighbours node)
           getNeighbours node = Monad.join . map (maybeToList . findInNodesMap) $ neighbours node
           findInNodesMap neighbour = Map.lookup neighbour nodesMap
           neighbours node = map (xor node) offsets ++ 
-                            map (\pair -> xor node ((pair !! 0) .|. (pair !! 1))) (combinationsOf 2 offsets)
+                            map (\(x:y:_) -> xor node (x .|. y)) (combinationsOf 2 offsets)
           smallestIdFirst (id1, id2) = if id1 > id2 then (id2, id1) else (id1, id2)
 
 -- findMaxClusters :: String -> Int
@@ -38,7 +41,8 @@ findMaxClusters fileContents =
     -- nodesToMerge nodes nodesMap (map (shiftL 1) [0..(bits - 1)])
     maxCluster bits nodes unionFind nodesMap
     where (bits,nodes) = process fileContents
-          unionFind = (create (0, length nodes-1)) 
+          -- unionFind = (emptyEquivalence (0, length nodes-1)) 
+          unionFind = (create (0, length nodes-1))           
           nodesMap = Map.fromList (zip nodes [0..])
 
 process :: String -> (Int, [Int])
