f8a17d84c299597232a28761aee688d408d6eb91
Mark Needham <m.h.needham@gmail.com>
2012-12-30 17:54:34 +0000

    mark: uncluttering the namespace

--- clustering.hs
+++ clustering.hs
@@ -5,31 +5,31 @@ import Data.Bits
 import Control.Monad
 import UnionFind
 
-import Data.Map
-import Data.List
-import Data.Maybe
-import Data.Set
+import qualified Data.Map as Map
+import qualified Data.List
+import qualified Data.Set as Set
+import Data.Maybe as Maybe
     
 file = "clustering2.txt"
 
 -- subsets of size k
 combinationsOf 0 _ = [[]]
 combinationsOf _ [] = []
-combinationsOf k (x:xs) = Prelude.map (x:) (combinationsOf (k-1) xs) ++ combinationsOf k xs
+combinationsOf k (x:xs) = map (x:) (combinationsOf (k-1) xs) ++ combinationsOf k xs
 
-maxCluster :: Int -> [Int] -> Equivalence Int -> Map Int Int -> Int
+maxCluster :: Int -> [Int] -> Equivalence Int -> Map.Map Int Int -> Int
 maxCluster bits nodes unionFind nodesMap = 
-    numberOfComponents $ Data.List.foldl (\uf (x,y) -> equate x y uf) unionFind (nodesToMerge nodes nodesMap offsets)
-    where offsets = Prelude.map (shiftL 1) [0..(bits - 1)]
+    numberOfComponents $ foldl (\uf (x,y) -> equate x y uf) unionFind (nodesToMerge nodes nodesMap offsets)
+    where offsets = map (shiftL 1) [0..(bits - 1)]
 
-nodesToMerge :: [Int] -> Map Int Int -> [Int] -> [(Int, Int)]
+nodesToMerge :: [Int] -> Map.Map Int Int -> [Int] -> [(Int, Int)]
 nodesToMerge nodes nodesMap offsets = 
     concatMap nodeCombinations (zip [0..] nodes)    
     where nodeCombinations (nodeIndex, node) = zip (repeat nodeIndex) (getNeighbours node)
-          getNeighbours node = join . Prelude.map (maybeToList . findInNodesMap) $ neighbours node
-          findInNodesMap neighbour = Data.Map.lookup neighbour nodesMap
-          neighbours node = Prelude.map (xor node) offsets ++ 
-                            Prelude.map (\pair -> xor node ((pair !! 0) .|. (pair !! 1))) (combinationsOf 2 offsets)
+          getNeighbours node = join . map (maybeToList . findInNodesMap) $ neighbours node
+          findInNodesMap neighbour = Map.lookup neighbour nodesMap
+          neighbours node = map (xor node) offsets ++ 
+                            map (\pair -> xor node ((pair !! 0) .|. (pair !! 1))) (combinationsOf 2 offsets)
 
 -- findMaxClusters :: String -> Int
 findMaxClusters fileContents = 
@@ -38,15 +38,15 @@ findMaxClusters fileContents =
     maxCluster bits nodes unionFind nodesMap
     where (bits,nodes) = process fileContents
           unionFind = (emptyEquivalence (0, length nodes-1)) 
-          nodesMap = Data.Map.fromList (zip nodes [0..])
+          nodesMap = Map.fromList (zip nodes [0..])
 
 process :: String -> (Int, [Int])
 process fileContents = (bits, nodes)
                        where bits = extractBits $ processedFileContents !! 0
-                             nodes = Data.Set.toList . Data.Set.fromList . Prelude.map (toDecimal . trimSpaces) . (drop 1) $ processedFileContents
+                             nodes = Set.toList . Set.fromList . map (toDecimal . trimSpaces) . (drop 1) $ processedFileContents
                              processedFileContents = splitOn "\n" fileContents
-                             trimSpaces = Prelude.filter (not . isSpace)
-                             toDecimal = Prelude.foldr (\c s -> s * 2 + c) 0 . reverse . Prelude.map digitToInt 
+                             trimSpaces = filter (not . isSpace)
+                             toDecimal = foldr (\c s -> s * 2 + c) 0 . reverse . map digitToInt 
                              extractBits header = read $ (splitOn " " header) !! 1 
 
 main = do     
