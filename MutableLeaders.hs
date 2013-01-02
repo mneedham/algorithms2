d8cb7aeabdc92f5c119a2f1b51c8d9e4ee1cee21
Mark Needham <m.h.needham@gmail.com>
2013-01-02 22:26:51 +0000

    mark: removing unused code

--- MutableLeaders.hs
+++ MutableLeaders.hs
@@ -21,10 +21,8 @@ import qualified Data.Set as Set
 --     b <- readArray arr 1 
 --     print (a,b)
     
--- data MutableUnionSet i = MutableUnionSet { leaders :: IO (IOArray Int Int) }    
     
 create :: (Int, Int) -> IO (IOArray Int Int)
--- create :: (Int, Int) -> MutableUnionSet i
 create (start, end) = do 
     actualArray <- newArray (start,end) 0
     ls <- getAssocs actualArray
@@ -43,24 +41,7 @@ inSameComponent arrayContainer x y = do
     xLeader <- readArray actualArray x
     yLeader <- readArray actualArray y
     return $ (xLeader == yLeader)
-
--- union :: IO (IOArray Int Int) -> Int -> Int -> IO (IOArray Int Int)
--- union arrayContainer x y = do
---     actualArray <- arrayContainer
---     ls <- getAssocs actualArray
---     leader1 <- readArray actualArray x
---     leader2 <- readArray actualArray y
---     
---     -- (start, end) <- getBounds actualArray
---     -- sequence $ map (\idx -> update arrayContainer idx leader1 leader2 ) [start..end]
---     -- [start..end]
---     
---     -- let newValues = map (\(index, value) -> if value == leader1 then (index, leader2) else (index, value)) ls
---     let newValues = (map (\(index, value) -> (index, leader1)) . filter (\(index, value) -> value == leader2)) ls
---     sequence $ map (\(idx, val) -> writeArray actualArray idx val) newValues
---     -- sequence $ map (\(idx, val) -> writeArray actualArray idx val) newValues
---     return actualArray         
-
+      
 findParent :: IOArray Int Int -> Int -> IO Int
 findParent actualArray index = do
     parent <- readArray actualArray index
@@ -79,23 +60,6 @@ union x y arrayContainer = do
     sequence $ map (\(idx, val) -> writeArray actualArray idx val) newValues
     return actualArray  
 
--- update :: [(Int, Int)] -> IO (IOArray Int Int) -> IO (IOArray Int Int)
--- update newValues arrayContainer = do
---     actualArray <- arrayContainer 
---     sequence $ map (\(idx, val) -> writeArray actualArray idx val) newValues
---     -- writeArray actualArray 0 64
---     -- values <- getAssocs actualArray
---     -- trace (show values) $ return actualArray
---     return actualArray
---     -- actual <- myArray
---     -- print(elems $ freeze actual)
---     -- return actual
-    
--- main = do
---     uf <- update [(0, 5), (3, 4)] $ create (0,10)
---     values <- getAssocs uf
---     putStrLn (show values)
-
 update :: IO (IOArray Int Int) -> Int -> Int -> Int -> IO (IOArray Int Int)
 update arrayContainer index newValue checkValue = do
      actualArray <- arrayContainer
@@ -104,6 +68,4 @@ update arrayContainer index newValue checkValue = do
          writeArray actualArray index newValue         
      else
          writeArray actualArray index lookedUpValue         
-     return actualArray
-    
-    
+     return actualArray    
