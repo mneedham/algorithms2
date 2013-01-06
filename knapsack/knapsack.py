5f4682d29efaf0f5af2b67f6876e84eb71147ef2
Mark Needham <m.h.needham@gmail.com>
2013-01-06 00:19:44 +0000

    mark: adding a python version which actually completes unlike the ruby one

---
+++ knapsack/knapsack.py
@@ -0,0 +1,45 @@
+import os
+
+def knapsack_cached(rows, knapsack_size, index):
+    global cache
+    if(index == 0 or knapsack_size == 0):
+        return 0
+    else:
+        value, weight = rows[index]
+        if(weight > knapsack_size):
+            if(knapsack_size not in cache[index-1]):
+                cache[index-1][knapsack_size] = knapsack_cached(rows, knapsack_size, index-1)
+                return cache[index-1][knapsack_size]
+            else: 
+                return cache[index-1][knapsack_size]
+        else:
+            if(knapsack_size not in cache[index-1]):
+                option_1  = knapsack_cached(rows, knapsack_size, index-1)
+                option_2  = value + knapsack_cached(rows, knapsack_size - weight, index-1)
+                cache[index-1][knapsack_size] = max(option_1, option_2)                
+                return cache[index-1][knapsack_size]
+            else:
+                return cache[index-1][knapsack_size]
+                    
+file = open(os.path.dirname(os.path.realpath(__file__)) + "/knapsack2.txt")
+
+knapsack_size, number_of_items = file.readline().replace("\n", "").split(" ")
+knapsack_size = int(knapsack_size)
+number_of_items = int(number_of_items)
+
+rows = []
+for line in file.readlines():
+    value, weight = line.split(" ")
+    rows.append([int(value), int(weight)])
+
+cache = []
+for i in range(0, number_of_items):
+    cache.append({})
+    
+result = knapsack_cached(rows, knapsack_size, number_of_items-1)    
+print(result)
+
+
+
+            
+
