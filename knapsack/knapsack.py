40d0724f32087035c984115c43d1dc465cb3e049
Mark Needham <m.h.needham@gmail.com>
2013-01-06 10:33:04 +0000

    mark: pass the cache around instead of having it global

--- knapsack/knapsack.py
+++ knapsack/knapsack.py
@@ -1,22 +1,21 @@
 import os
 
-def knapsack_cached(rows, knapsack_size, index):
-    global cache
+def knapsack_cached(rows, knapsack_size, index, cache):
     if(index is 0 or knapsack_size is 0):
         return 0
     else:
         value, weight = rows[index]
         if(weight > knapsack_size and knapsack_size not in cache[index-1]):
-            cache[index-1][knapsack_size] = knapsack_cached(rows, knapsack_size, index-1)                
+            cache[index-1][knapsack_size] = knapsack_cached(rows, knapsack_size, index-1, cache)
         else:
             if(knapsack_size not in cache[index-1]):
-                option_1  = knapsack_cached(rows, knapsack_size, index-1)
-                option_2  = value + knapsack_cached(rows, knapsack_size - weight, index-1)
+                option_1  = knapsack_cached(rows, knapsack_size, index-1, cache)
+                option_2  = value + knapsack_cached(rows, knapsack_size - weight, index-1, cache)
                 cache[index-1][knapsack_size] = max(option_1, option_2)                
             
         return cache[index-1][knapsack_size]
                     
-file = open(os.path.dirname(os.path.realpath(__file__)) + "/knapsack2.txt")
+file = open(os.path.dirname(os.path.realpath(__file__)) + "/knapsack1.txt")
 
 knapsack_size, number_of_items = map(lambda x: int(x), file.readline().replace("\n", "").split(" "))
 
@@ -29,5 +28,5 @@ cache = []
 for i in range(0, number_of_items):
     cache.append({})
     
-result = knapsack_cached(rows, knapsack_size, number_of_items-1)    
+result = knapsack_cached(rows, knapsack_size, number_of_items-1, cache)    
 print(result)
