c6ef69eee2c4ca570665a25eaf98f96a4b714278
Mark Needham <m.h.needham@gmail.com>
2013-01-17 22:57:39 +0000

    mark: checking in the attempts to optimise knapsack

--- knapsack/knapsack.hs
+++ knapsack/knapsack.hs
@@ -12,13 +12,15 @@ import qualified Data.HashTable.IO as H
 
 type HashTable k v = H.BasicHashTable k v
 
+
 foo :: IO (HashTable Int Int)
 foo = do
     ht <- H.new
     H.insert ht 1 1
+    value <- H.lookup ht 1
+    putStrLn $ (show $ fromJust value)
     return ht
-    
- 
+     
 ref :: a -> IORef a
 ref x = unsafePerformIO (newIORef x)    
                               
@@ -46,6 +48,43 @@ knapsackCached rows weight numberOfItems =
                                          (vi:wi:_) = rows !! i in 
                                      if wi > w then best
                                      else maximum [best, vi + inner (i-1, w-wi)])
+
+-- memoize :: ((Int, Int) -> IO Int) -> (Int, Int) -> IO Int                  
+-- memoize fn mapKey = unsafePerformIO $ do 
+--   let cache = ref (Map.empty :: Map.Map (Int, Int) Int)
+--   items <- readIORef cache  
+--   let lookupValue = Map.lookup mapKey items
+--   if lookupValue  == Nothing then do    
+--     result <- fn mapKey
+--     writeIORef cache $  Map.insert mapKey result items
+--     trace ((show items)) $ return $ return result
+--   else do
+--     let result = fromJust $ Map.lookup mapKey items
+--     trace (show lookupValue) $ return $ return result
+--         
+-- knapsackCached :: [[Int]] -> Int -> Int -> IO Int
+-- knapsackCached rows weight numberOfItems = do
+--   inner (numberOfItems-1, weight)
+--   -- return 0
+--   where inner = memoize (\(i,w) -> if i < 0 || w == 0 then return 0
+--                                    else do
+--                                      best <- inner (i-1,w)
+--                                      let (vi:wi:_) = rows !! i
+--                                      if wi > w then return best
+--                                      else do 
+--                                        result <- inner (i-1, w-wi)
+--                                        return $ maximum [best, vi + result])
+
+-- inner = memoize (\(i,w) -> if i < 0 || w == 0 then do return 0
+--                                  else do
+--                                    best <- inner (i-1,w)
+--                                    let (vi:wi:_) = [] !! i
+--                                        -- best = 4
+--                                    if wi > w then return best
+--                                    else do 
+--                                      -- let result = 4 
+--                                      result <- inner (i-1, w-wi)
+--                                      return $ maximum [best, vi + result])
 
 knapsackCached1 :: [[Int]] -> Int -> Int -> IORef (Map.Map (Int, Int) Int) -> Int
 knapsackCached1 rows knapsackWeight index cacheContainer = unsafePerformIO $ do
@@ -83,5 +122,9 @@ main = do
     contents <- readFile (args !! 0)
     let (knapsackSize, numberOfItems, rows) = process contents
         cache = ref (Map.empty :: Map.Map (Int, Int) Int)
+    let result = knapsackCached rows knapsackSize numberOfItems
     -- putStrLn $ show $ knapsackCached1 rows knapsackSize (numberOfItems-1) cache
-    putStrLn $ show $ knapsackCached rows knapsackSize numberOfItems
+    putStrLn $ show $ result
+    
+-- main = do 
+--   foo
