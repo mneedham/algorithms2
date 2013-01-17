ac0c50c988b4ee174892bac2edbf8620a03b97ba
Mark Needham <m.h.needham@gmail.com>
2013-01-17 22:10:12 +0000

    mark: now detecting negative cycles

--- shortestpath/shortestpaths-simple.py
+++ shortestpath/shortestpaths-simple.py
@@ -1,9 +1,21 @@
 import os
 from numpy import *
+from time import time
+import sys
 
-file = open(os.path.dirname(os.path.realpath(__file__)) + "/g3.txt")
+start_time = time()
+
+file = open(os.path.dirname(os.path.realpath(__file__)) + "/" + sys.argv[1:][0])
 vertices, edges = map(lambda x: int(x), file.readline().replace("\n", "").split(" "))
 rows = []
+
+adjacency_matrix = zeros((vertices, vertices))
+adjacency_matrix[:] = float("inf")
+for line in file.readlines():
+    tail, head, weight = line.split(" ")
+    adjacency_matrix[int(head)-1][int(tail)-1] = int(weight)
+
+print("processed file: " + str(time() - start_time))
 
 def initialise_cache(vertices, s):
     cache = empty(vertices)
@@ -11,25 +23,33 @@ def initialise_cache(vertices, s):
     cache[s] = 0
     return cache    
 
-adjacency_matrix = zeros((vertices, vertices))
-adjacency_matrix[:] = float("inf")
-for line in file.readlines():
-    tail, head, weight = line.split(" ")
-    adjacency_matrix[int(head)-1][int(tail)-1] = int(weight)    
-
 shortest_paths = []
 
-for s in range(0, 1):
+for s in range(0, vertices):
     cache = initialise_cache(vertices, s)
     for i in range(1, vertices):
-        previous_cache = cache
+        # print("iteration" + str(i) + ": " + str(time() - start_time))
+        previous_cache = cache[:]
+                
         combined = (previous_cache.T + adjacency_matrix).min(axis=1)
         cache = minimum(previous_cache, combined)
-
+        
+        if(alltrue(cache == previous_cache)):
+            break;
+    
+    # checking for negative cycles
+    previous_cache = cache[:]
+    combined = (previous_cache.T + adjacency_matrix).min(axis=1)
+    cache = minimum(previous_cache, combined)
+    
+    print("s: " + str(s) + " done " + str(time() - start_time))
+    if(not alltrue(cache == previous_cache)):
+        raise Exception("negative cycle bitches")
+            
     shortest_paths.append([s, cache])
+    
+all_shortest = reduce(lambda x, y: concatenate((x,y), axis=1), map(lambda x: x[1], shortest_paths))
+print(min(all_shortest))
 
-# for path in shortest_paths:
-#     print(str(path[1]))
-
-shortest_path = min(reduce(lambda x, y: x + y, map(lambda x: x[1], shortest_paths)))
-print("Shortest Path: " + str(shortest_path))  
+# shortest_path = min(reduce(lambda x, y: x + y, map(lambda x: x[1], shortest_paths)))
+# print("Shortest Path: " + str(shortest_path))  
